ac47fdb9033864b97b289ace187ca1d64a5e4344
developeranass <developeranass>
2026-07-10 16:56:39 +0500

    Testing deployments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/',(req,res) => {
-    return res.json({  msg : 'Hello from server testing Again' })
+    return res.json({  msg : 'Hello My name is anas sartaj and i am checking deployments' })
 })
 
 console.log(process.env.PORT);
